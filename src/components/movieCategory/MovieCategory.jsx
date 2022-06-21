3c9c8c7f991a468da68ec49c667f766721a0d153
Tiffany <yang.ding@hotmail.com>
2022-06-20 20:00:46 -0700

    set up MovieCategory component

---
+++ src/components/movieCategory/MovieCategory.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {} from './style'
+
+const MovieCategory = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default MovieCategory
